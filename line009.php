ddb6c6b2ffc347ffe606657f58d001cbce6da891
Gbike <59223522+Gbike@users.noreply.github.com>
2020-01-22 17:17:16 +0700

    Update line009.php

--- line009.php
+++ line009.php
@@ -21,7 +21,7 @@ if($name<>"" || $email <> "" || $mesg <> "") {
 $res = notify_message($message,$stickerPkg,$stickerId,$token);
  echo "<center>ส่งข้อความเรียบร้อยแล้ว</center>";
  ?>
-<meta http-equiv="refresh" content="2;url=indexhtml">
+<meta http-equiv="refresh" content="2;url=index.html">
 <?php
 
 } else {
